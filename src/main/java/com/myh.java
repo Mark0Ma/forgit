557d847ef73558881c5245a4f9c51cd4596d39dc
myh <1172259344@qq.com>
2023-08-20 02:04:31 +0800

    second-commit-idea

--- src/main/java/com/myh.java
+++ src/main/java/com/myh.java
@@ -3,5 +3,10 @@ package com;
 public class myh {
     public static void main(String[] args) {
         System.out.println("hello_world");
+
+        int i = 1;
+        System.out.println(i);
+
+
     }
 }
